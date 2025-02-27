5cf04c5ed139c5b24fe746c72fd5812d5bccf059
Pablofd <pablo.fernandezdiaz@edu.uah.es>
2025-02-27 19:23:08 +0100

    Intento de Test(No funciona) da un error raro preguntar al profe

---
+++ Test/java/ColaTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColaTest {
+    @Test
+    void testPilaYCola() {
+        Pila<Integer> pila = new Pila<>();
+        pila.push(1);
+        pila.push(2);
+        assertEquals(2, pila.pop());
+        Cola<Integer> cola = new Cola<>();
+        cola.enqueue(10);
+        cola.enqueue(20);
+        assertEquals(10, cola.dequeue());
+    }
+}
