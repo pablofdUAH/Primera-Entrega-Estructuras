c59b8e24de5a4c49456e7d713585b48c39796034
marioo_ra <mariora.0013@gmail.com>
2025-03-05 11:43:00 +0100

    Comentarios iteradores, elementos e interfaces

--- src/Iterador.java
+++ src/Iterador.java
@@ -1,5 +1,14 @@
 public interface Iterador<T> {
+    // ZONA DE ATRIBUTOS
     boolean hasNext();
+
+
+    // ZONA DE METODOS
+        // Constructor
+
+        // Métodos de la interfaz
+
+        // Métodos propios
     T next();
     void delete();
 }
